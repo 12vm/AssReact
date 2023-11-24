c2bf2e38755c72fdebc29b0690b329ffd68b8bb0
VEID KRISHNA MISHRA <100232581+12vm@users.noreply.github.com>
2023-11-25 03:18:51 +0530

    Create Render.js

---
+++ Assign 1 React/src/Render.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {arr} from './components/data';
+import UserList from './components/UserList';
+import UserTable from './components/UserTable';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
